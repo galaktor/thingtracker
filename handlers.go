5de2605a048a9dfe27ab690db26b613a81949cd1
restrada <raphael.estrada@intel.com>
2015-08-18 12:21:15 +0100

    basic rendering switch, template files

--- handlers.go
+++ handlers.go
@@ -1,37 +1,63 @@
 package main
 
 import (
-//	"encoding/json"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
 	"time"
+	"io/ioutil"
+	"path/filepath"
+	"io"
 
 	"github.com/gorilla/mux"
 )
 
-var list = `<html>
-  <body>
-    <ul>
-    foo
-    {{range $i, $t := .}}
-      <li>Id: {{$t.Id}}, Title: {{$t.Title}}</li>
-    {{end}}
-    </ul>
-  </body>
-</html>`
+var thing_list *template.Template
+var thing_single *template.Template
 
-var one = `<html>
-  <body>
-    <p>Id: {{.Id}}</p>
-    <p>Title: {{.Title}}</p>
-    <p>Description: {{.Description}}</p>
-    <p>Due: {{.Due}}</p>
-    <p>ThingName: {{.ThingName}}</p>
-    <p>ThingLink: {{.ThingLink}}</p>
-  </body>
-</html>`
+var things []Thing
 
+func init() {
+	loadTemplates()
+	t, err := getThings()
+	guard(err)
+	things = t
+}
+
+func loadTemplates() {
+	file, err := ioutil.ReadFile("thing_single.tmpl")
+	guard(err)
+	thing_single, err = template.New("thing_single").Parse(string(file))
+	guard(err)
+
+	file, err = ioutil.ReadFile("thing_list.tmpl")
+	guard(err)
+	thing_list, err = template.New("thing_list").Parse(string(file))
+	guard(err)
+}
+
+func guard(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getThings() ([]Thing, error) {
+	files, err := ioutil.ReadDir("store")
+	guard(err)
+
+	out := []Thing{}
+	for _, file := range files {
+		if file.Mode().IsRegular() {
+			if filepath.Ext(file.Name()) == "*.thing" {
+				out = append(out, Thing{})
+			}
+		}
+	}
+	
+	return out, err	
+}
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome!")
@@ -43,17 +69,32 @@ func Things(w http.ResponseWriter, r *http.Request) {
 		Thing{Id: "2", Title: "two", ThingName: "Bar", ThingLink: "http://www.bar.foo"},
 	}
 
-	tmpl := template.New("things")
-	var err error
+	renderJson(w, things)
+}
 
-	if tmpl, err = tmpl.Parse(list); err != nil {
-		http.Error(w, err.Error(), 500)
+func renderJson(out io.Writer, i interface{}) error {
+	if err := json.NewEncoder(out).Encode(i); err != nil {
+		return err
 	}
-	tmpl.Execute(w, things)
 
-/*	if err := json.NewEncoder(w).Encode(things); err != nil {
-		panic(err)
-	}*/
+	return nil
+}
+
+func renderHtml(out io.Writer, t *template.Template, i interface{}) error {
+	if err := t.Execute(out, i); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getMimetype(r *http.Request) string {
+	mimetype := mux.Vars(r)["mimetype"]
+	if mimetype == "" {
+		mimetype = "html"
+	}
+
+	return mimetype
 }
 
 func SingleThing(w http.ResponseWriter, r *http.Request) {
@@ -68,11 +109,8 @@ func SingleThing(w http.ResponseWriter, r *http.Request) {
 
 	// TODO deserialize struct from json file with id in name
 
-	tmpl := template.New("thing")
-	var err error
-
-	if tmpl, err = tmpl.Parse(one); err != nil {
-		http.Error(w, err.Error(), 500)
+	switch(getMimetype(r)) {
+	case "html": renderHtml(w, thing_single, thing)
+	case "json": renderJson(w, thing)
 	}
-	tmpl.Execute(w, thing)
 }
